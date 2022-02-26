3be9fb7c1b5fb5d25cc5f101bb564ed86234b9c5
SirObby <70753342+SirObby@users.noreply.github.com>
2022-02-26 20:51:47 +0800

    yes

--- package-repo/get.php
+++ package-repo/get.php
@@ -3,12 +3,13 @@
 # Get query from URL
 $query = $_GET['pkg'];
 
-# Search if a file exists in the folder with the name of the package
-# And return the path to the file as a JSON object
-if (file_exists("./$query")) {
-    echo json_encode(array("path" => "./$query"));
-} else {
-    echo json_encode(array("error" => "Package not found"));
+# Search if a file contains $query if so, return the file
+$files = glob('*');
+foreach ($files as $file) {
+    if (strpos($file, $query) !== false) {
+        echo $file;
+        exit;
+    }
 }
 
 ?>
